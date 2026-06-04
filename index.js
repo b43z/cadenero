16918fdfcb3c9054891e5841d2b89485250ec910
b43z <87758137+b43z@users.noreply.github.com>
2026-06-03 20:12:17 -0600

    Simplify bracket escaping in escapeMarkdownV2
    Refactor escapeMarkdownV2 function to simplify bracket escaping.

--- index.js
+++ index.js
@@ -86,16 +86,8 @@ function escapeMarkdownV2(text) {
   return text
     .replace(/_/g, "\\_")
     .replace(/\*/g, "\\*")
-    .replace(/
-
-\[/g, "\
-
-\[")
-    .replace(/\]
-
-/g, "\\]
-
-")
+    .replace(/\[/g, "\\[")
+    .replace(/\]/g, "\\]")
     .replace(/\(/g, "\\(")
     .replace(/\)/g, "\\)")
     .replace(/~/g, "\\~")
